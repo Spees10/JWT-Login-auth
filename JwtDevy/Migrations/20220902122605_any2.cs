1689e7972b7bb952034d07ebee7966466b48f5da
spees10 <Spees.lm@gmail.com>
2022-09-24 12:54:58 +0200

    first c

---
+++ JwtDevy/Migrations/20220902122605_any2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JwtDevy.Migrations
+{
+    public partial class any2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
